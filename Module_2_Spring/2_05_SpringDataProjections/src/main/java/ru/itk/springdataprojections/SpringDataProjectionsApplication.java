b179bcd5bc551c695cb34c4118348338caa81e39
Daniil Futin <futinde@gmail.com>
2025-12-09 21:38:32 +0300

    Initialized the application

---
+++ Module_2_Spring/2_05_SpringDataProjections/src/main/java/ru/itk/springdataprojections/SpringDataProjectionsApplication.java
@@ -0,0 +1,13 @@
+package ru.itk.springdataprojections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataProjectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataProjectionsApplication.class, args);
+    }
+
+}
