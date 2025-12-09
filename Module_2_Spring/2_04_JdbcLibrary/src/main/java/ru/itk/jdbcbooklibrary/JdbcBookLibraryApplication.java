da5e2bc7560d1a3a3a135212ff96a465f10f828d
Daniil Futin <futinde@gmail.com>
2025-12-09 09:53:09 +0300

    Initialized the application

---
+++ Module_2_Spring/2_04_JdbcLibrary/src/main/java/ru/itk/jdbcbooklibrary/JdbcBookLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.itk.jdbcbooklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcBookLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcBookLibraryApplication.class, args);
+	}
+
+}
